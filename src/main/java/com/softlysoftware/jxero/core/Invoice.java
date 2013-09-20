1b5963b53672c01435dda92431fe45cb87f89a03
Keith Harris <keith@softlysoftware.com>
2013-09-20 20:18:59 +0100

    Returned file to original formatting so that we could see the details of this change (via git diff 4e68f577f02f58f9981bbcbd4b40367a3bd3033e src/main/java/com/softlysoftware/jxero/core/Invoice.java) ... the new amountDue property.

--- src/main/java/com/softlysoftware/jxero/core/Invoice.java
+++ src/main/java/com/softlysoftware/jxero/core/Invoice.java
@@ -38,248 +38,128 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.NONE)
 public class Invoice {
 
-  // TODO - Parse using the Xero Account's timezone
-  private static SimpleDateFormat DF = new SimpleDateFormat("yyyy-MM-dd'T00:00:00'");
-  static {
-    DF.setTimeZone(TimeZone.getTimeZone("UTC"));
-  }
+	// TODO - Parse using the Xero Account's timezone
+	private static SimpleDateFormat DF = new SimpleDateFormat("yyyy-MM-dd'T00:00:00'");
+	static {DF.setTimeZone(TimeZone.getTimeZone("UTC"));}
 
-  private static NumberFormat MONEY_FORMAT = new DecimalFormat("0.00");
+	private static NumberFormat MONEY_FORMAT = new DecimalFormat("0.00");
 
-  @XmlElement(name = "InvoiceID")
-  private String id;
+	@XmlElement(name = "InvoiceID")
+	private String id;
+	public String getId(){return id;}
+	public void setId(String id){this.id = id;}
 
-  public String getId() {
-    return id;
-  }
+	@XmlElement(name = "InvoiceNumber")
+	private String number;
+	public String getNumber(){return number;}
+	public void setNumber(String number){this.number = number;}
 
-  public void setId(String id) {
-    this.id = id;
-  }
+	public enum Type {ACCPAY, ACCREC};
 
-  @XmlElement(name = "InvoiceNumber")
-  private String number;
+	@XmlElement(name = "Type")
+	private String type;
 
-  public String getNumber() {
-    return number;
-  }
+	@XmlElement(name = "AmountDue")
+	private BigDecimal amountDue;
+	public BigDecimal getAmountDue() {return amountDue;}
+	public void setAmountDue(BigDecimal amountDue) {this.amountDue = amountDue;}
 
-  public void setNumber(String number) {
-    this.number = number;
-  }
+	public Type getType() {
+		if (type == null) return null;
+		if (type.equals("ACCPAY")) return Type.ACCPAY;
+		if (type.equals("ACCREC")) return Type.ACCREC;
+		throw new RuntimeException("Bad Invoice type : " + type);
+	}
+	public void setType(Type type) {this.type = type.toString();}
 
-  public enum Type {
-    ACCPAY,
-    ACCREC
-  };
+	@XmlElement(name = "Contact")
+	private Contact contact;
+	public Contact getContact(){return contact;}
+	public void setContact(Contact contact){this.contact = contact;}
 
-  @XmlElement(name = "Type")
-  private String type;
-  
-  @XmlElement(name = "AmountDue")
-  private BigDecimal amountDue;
+	@XmlElement(name = "Date")
+	private String date;
+	public Date getDate(){
+		try {return DF.parse(date);}
+		catch (ParseException e) {throw new RuntimeException(e);}
+	}
+	public void setDate(Date date){this.date = DF.format(date);}
 
-  public Type getType() {
-    if (type == null)
-      return null;
-    if (type.equals("ACCPAY"))
-      return Type.ACCPAY;
-    if (type.equals("ACCREC"))
-      return Type.ACCREC;
-    throw new RuntimeException("Bad Invoice type : " + type);
-  }
+	@XmlElement(name = "DueDate")
+	private String dueDate;
+	public Date getDueDate(){
+		try {return DF.parse(dueDate);}
+		catch (ParseException e) {throw new RuntimeException(e);}
+	}
+	public void setDueDate(Date dueDate){this.dueDate = DF.format(dueDate);}
 
-  public void setType(Type type) {
-    this.type = type.toString();
-  }
+	@XmlElement(name = "Reference")
+	private String reference;
+	public String getReference(){return reference;}
+	public void setReference(String reference){this.reference = reference;}
 
-  @XmlElement(name = "Contact")
-  private Contact contact;
+	@XmlElement(name = "BrandingThemeID")
+	private String brandingThemeID;
+	public String getBrandingThemeID(){return brandingThemeID;}
+	public void setBrandingThemeID(String brandingThemeID){this.brandingThemeID = brandingThemeID;}
 
-  public Contact getContact() {
-    return contact;
-  }
+	@XmlElement(name = "Url")
+	private String url;
+	public String getUrl(){return url;}
+	public void setUrl(String url){this.url = url;}
 
-  public void setContact(Contact contact) {
-    this.contact = contact;
-  }
+	@XmlElement(name = "CurrencyCode")
+	private String currencyCode;
+	public String getCurrencyCode(){return currencyCode;}
+	public void setCurrencyCode(String currencyCode){this.currencyCode = currencyCode;}
 
-  @XmlElement(name = "Date")
-  private String date;
+	public enum Status {DRAFT, SUBMITTED, AUTHORISED, PAID};
 
-  public Date getDate() {
-    try {
-      return DF.parse(date);
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
-    }
-  }
+	@XmlElement(name = "Status")
+	private String status;
+	public Status getStatus(){
+		if (status == null) return null;
+		if (status.equals("DRAFT")) return Status.DRAFT;
+		if (status.equals("SUBMITTED")) return Status.SUBMITTED;
+		if (status.equals("AUTHORISED")) return Status.AUTHORISED;
+		if (status.equals("PAID")) return Status.PAID;
+		throw new RuntimeException("Bad status : " + status);
+	}
+	public void setStatus(Status status){this.status = status.toString();}
 
-  public void setDate(Date date) {
-    this.date = DF.format(date);
-  }
+	@XmlElement(name = "LineAmountTypes")
+	private String lineAmountTypes;
+	public String getLineAmountTypes(){return lineAmountTypes;}
+	public void setLineAmountTypes(String lineAmountTypes){this.lineAmountTypes = lineAmountTypes;}
 
-  @XmlElement(name = "DueDate")
-  private String dueDate;
+	@XmlElement(name = "SubTotal")
+	private String subTotal;
+	public double getSubTotal(){
+		try {return MONEY_FORMAT.parse(subTotal).doubleValue();}
+		catch (ParseException e) {throw new RuntimeException(e);}
+	}
+	public void setSubTotal(double subTotal){this.subTotal = MONEY_FORMAT.format(subTotal);} 
 
-  public Date getDueDate() {
-    try {
-      return DF.parse(dueDate);
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
-    }
-  }
+	@XmlElement(name = "TotalTax")
+	private String totalTax;
+	public double getTotalTax(){
+		try {return MONEY_FORMAT.parse(totalTax).doubleValue();}
+		catch (ParseException e) {throw new RuntimeException(e);}
+	}
+	public void setTotalTax(double totalTax){this.totalTax = MONEY_FORMAT.format(totalTax);} 
 
-  public void setDueDate(Date dueDate) {
-    this.dueDate = DF.format(dueDate);
-  }
+	@XmlElement(name = "Total")
+	private String total;
+	public double getTotal(){
+		try {return MONEY_FORMAT.parse(total).doubleValue();}
+		catch (ParseException e) {throw new RuntimeException(e);}
+	}
+	public void setTotal(double total){this.total = MONEY_FORMAT.format(total);}
 
-  @XmlElement(name = "Reference")
-  private String reference;
-
-  public String getReference() {
-    return reference;
-  }
-
-  public void setReference(String reference) {
-    this.reference = reference;
-  }
-
-  @XmlElement(name = "BrandingThemeID")
-  private String brandingThemeID;
-
-  public String getBrandingThemeID() {
-    return brandingThemeID;
-  }
-
-  public void setBrandingThemeID(String brandingThemeID) {
-    this.brandingThemeID = brandingThemeID;
-  }
-
-  @XmlElement(name = "Url")
-  private String url;
-
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
-  @XmlElement(name = "CurrencyCode")
-  private String currencyCode;
-
-  public String getCurrencyCode() {
-    return currencyCode;
-  }
-
-  public void setCurrencyCode(String currencyCode) {
-    this.currencyCode = currencyCode;
-  }
-
-  public enum Status {
-    DRAFT,
-    SUBMITTED,
-    AUTHORISED,
-    PAID
-  };
-
-  @XmlElement(name = "Status")
-  private String status;
-
-  public Status getStatus() {
-    if (status == null)
-      return null;
-    if (status.equals("DRAFT"))
-      return Status.DRAFT;
-    if (status.equals("SUBMITTED"))
-      return Status.SUBMITTED;
-    if (status.equals("AUTHORISED"))
-      return Status.AUTHORISED;
-    if (status.equals("PAID"))
-      return Status.PAID;
-    throw new RuntimeException("Bad status : " + status);
-  }
-
-  public void setStatus(Status status) {
-    this.status = status.toString();
-  }
-
-  @XmlElement(name = "LineAmountTypes")
-  private String lineAmountTypes;
-
-  public String getLineAmountTypes() {
-    return lineAmountTypes;
-  }
-
-  public void setLineAmountTypes(String lineAmountTypes) {
-    this.lineAmountTypes = lineAmountTypes;
-  }
-
-  @XmlElement(name = "SubTotal")
-  private String subTotal;
-
-  public double getSubTotal() {
-    try {
-      return MONEY_FORMAT.parse(subTotal).doubleValue();
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public void setSubTotal(double subTotal) {
-    this.subTotal = MONEY_FORMAT.format(subTotal);
-  }
-
-  @XmlElement(name = "TotalTax")
-  private String totalTax;
-
-  public double getTotalTax() {
-    try {
-      return MONEY_FORMAT.parse(totalTax).doubleValue();
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public void setTotalTax(double totalTax) {
-    this.totalTax = MONEY_FORMAT.format(totalTax);
-  }
-
-  @XmlElement(name = "Total")
-  private String total;
-
-  public double getTotal() {
-    try {
-      return MONEY_FORMAT.parse(total).doubleValue();
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public void setTotal(double total) {
-    this.total = MONEY_FORMAT.format(total);
-  }
-
-  @XmlElementWrapper(name = "LineItems")
-  @XmlElement(name = "LineItem")
-  private List<LineItem> lineItems = new LinkedList<LineItem>();
-
-  public List<LineItem> getLineItems() {
-    return lineItems;
-  }
-
-  public void setLineItems(List<LineItem> lineItems) {
-    this.lineItems = lineItems;
-  }
-
-  public BigDecimal getAmountDue() {
-    return amountDue;
-  }
-
-  public void setAmountDue(BigDecimal amountDue) {
-    this.amountDue = amountDue;
-  }
+	@XmlElementWrapper(name = "LineItems")
+	@XmlElement(name = "LineItem")
+	private List<LineItem> lineItems = new LinkedList<LineItem>();
+	public List<LineItem> getLineItems(){return lineItems;}
+	public void setLineItems(List<LineItem> lineItems){this.lineItems = lineItems;}
 
 }
